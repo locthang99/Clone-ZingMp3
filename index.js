36e517bc06a89b023f77f0648c5e917853a647f0
Loc Thang <ldthang@tma.com.vn>
2021-04-12 17:38:12 +0700

    up

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ async function rmDup(path, outPath) {
   var checkList = new Map();
   var count = 0;
   await lineReader.eachLine(path, function (line) {
-    //console.log(line.slice(7,15)) art
 
     if (!checkList[line.slice(13, 23)]) {
       //console.log(obj)
@@ -37,102 +36,105 @@ async function rmDup(path, outPath) {
 }
 
 var listTypes = [
-  "t-",
+  "-",
   "viet-nam-cai-luong-",
-  "viet-nam-nhac-tre-",
-  "au-my-classical-",
-  "au-my-folk-",
-  "au-my-country-",
-  "au-my-pop-",
-  "au-my-rock-",
+  "viet-nam-nhac-tre-v-pop-",
   "viet-nam-nhac-trinh-",
   "viet-nam-nhac-tru-tinh-",
   "viet-nam-rap-viet-",
   "viet-nam-nhac-thieu-nhi-",
   "viet-nam-nhac-cach-mang-",
   "viet-nam-nhac-dan-ca-que-huong-",
+  "viet-nam-nhac-ton-giao-",
+  "au-my-classical-",
+  "au-my-folk-",
+  "au-my-country-",
+  "au-my-pop-",
+  "au-my-rock-",
   "au-my-latin-",
   "au-my-rap-hip-hop-",
-  "au-my-rap-hip-hop-",
+  "au-my-alternative-",
   "au-my-blues-jazz-",
+  "au-my-reggae-",
+  "au-my-r-b-soul-",
   "trung-quoc-",
   "han-quoc-",
   "nhat-ban-",
   "thai-lan-",
-  "au-my-alternative-",
 ];
 
-// for(let i = 1 ;i<=6;i++)
-// {
-//   fs.readdir("Input"+"/clonezingbypython"+i+"/Data", (err, files) => {
-//     files.forEach(file => {
-//       //console.log(file);
-//       if(listTest.includes(file))
-//         rmDup("Input"+"/clonezingbypython"+i+"/Data/"+file,file)
-//     });
-//   });
-// }
 
-//rmDup("C:/Users/KhunGLonG/Desktop/clone/Cover/NotVN/thai-lan-","thai-lan-")
-
-// fs.readdir("DataJSON", (err, files) => {
-//   files.forEach((file) => {
-//     var path = ("Download/"+file).replace(".txt","")
-//     if (!fs.existsSync(path)) {
-//       fs.mkdirSync(path);
-//     }
-
-//     // if (listTest.includes(file))
-//     //   rmDup("Input" + "/clonezingbypython" + i + "/Data/" + file, file);
-//   });
-// });
-
-const request = require("request");
-const download = async (id,output) => {
-  try {
-    await request("http://api.mp3.zing.vn/api/streaming/audio/"+id+"/128").pipe(fs.createWriteStream(output));
-  } catch (err) {
-    console.log("toang");
-  }
-};
-
-function DownManyFromFile(i) {
-  var r = /Z[O|W][0-9|A-F|U|I|O|Z|W]{6}/g;
-  lineReader.eachLine("DataJSON/"+listTypes[i]+".txt", function (line) {
-    let rs1 = r.test(line.slice(14, 22));
-    let rs2 = r.test(line.slice(13, 21))
-        if (rs1==true) {
-          console.log(line.slice(14, 22) + " true14");
-          let id =line.slice(14, 22)
-          download(id,"Download/"+listTypes[i]+"/"+id+".mp3").then(()=>{console.log("done")})
-          return;
-        }
-        if (rs2==true) {
-          console.log(line.slice(13, 21) + " true13");
-          let id =line.slice(13, 21)
-          download(id,"Download/"+listTypes[i]+"/"+id+".mp3").then(()=>{console.log("done")})
-          return;
-        }
-    
+for(let i = 1 ;i<=6;i++)
+{
+  fs.readdir("Input"+"/clonezingbypython"+i+"/Data", (err, files) => {
+    files.forEach(file => {
+      //console.log(file);
+      if(listTest.includes(file))
+        rmDup("Input"+"/clonezingbypython"+i+"/Data/"+file,file)
+    });
   });
 }
 
+rmDup("C:/Users/KhunGLonG/Desktop/clone/Cover/NotVN/thai-lan-","thai-lan-")
 
-const fetch = require("node-fetch")
-fetch("https://604f32afc20143001744c8aa.mockapi.io/api/v1/config/cf").then(res=>res.json())
-.then(res=>{
-    if(!fs.existsSync("Download"))
-    {
-      fs.mkdirSync("Download");
+fs.readdir("DataJSON", (err, files) => {
+  files.forEach((file) => {
+    var path = ("Download/"+file).replace(".txt","")
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
     }
-      if (!fs.existsSync("Download/"+listTypes[res.id])) {
-      fs.mkdirSync("Download/"+listTypes[res.id]);
-    }
-    DownManyFromFile(res.id);
-})
-//var r =/Z[O|W][0-9|A-F|U|I|O|Z|W]{6}/g
 
-// for(let i = 0;i<100;i++)
-// {
-//   download("ZWF09OZ7",i+".mp3")
+    // if (listTest.includes(file))
+    //   rmDup("Input" + "/clonezingbypython" + i + "/Data/" + file, file);
+  });
+});
+
+// const request = require("request");
+// const download = async (id,output) => {
+//   try {
+//     await request("http://api.mp3.zing.vn/api/streaming/audio/"+id+"/128").pipe(fs.createWriteStream(output));
+//   } catch (err) {
+//     console.log("toang");
+//   }
+// };
+
+// function DownManyFromFile(i) {
+//   var r = /Z[O|W][0-9|A-F|U|I|O|Z|W]{6}/g;
+//   lineReader.eachLine("DataJSON/"+listTypes[i]+".txt", function (line) {
+//     let rs1 = r.test(line.slice(14, 22));
+//     let rs2 = r.test(line.slice(13, 21))
+//         if (rs1==true) {
+//           console.log(line.slice(14, 22) + " true14");
+//           let id =line.slice(14, 22)
+//           download(id,"Download/"+listTypes[i]+"/"+id+".mp3").then(()=>{console.log("done")})
+//           return;
+//         }
+//         if (rs2==true) {
+//           console.log(line.slice(13, 21) + " true13");
+//           let id =line.slice(13, 21)
+//           download(id,"Download/"+listTypes[i]+"/"+id+".mp3").then(()=>{console.log("done")})
+//           return;
+//         }
+    
+//   });
 // }
+
+
+// const fetch = require("node-fetch")
+// fetch("https://604f32afc20143001744c8aa.mockapi.io/api/v1/config/cf").then(res=>res.json())
+// .then(res=>{
+//     if(!fs.existsSync("Download"))
+//     {
+//       fs.mkdirSync("Download");
+//     }
+//       if (!fs.existsSync("Download/"+listTypes[res.id])) {
+//       fs.mkdirSync("Download/"+listTypes[res.id]);
+//     }
+//     DownManyFromFile(res.id);
+// })
+// //var r =/Z[O|W][0-9|A-F|U|I|O|Z|W]{6}/g
+
+// // for(let i = 0;i<100;i++)
+// // {
+// //   download("ZWF09OZ7",i+".mp3")
+// // }
