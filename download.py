89c657a55ffc98ef177c052c5acf169243199e7d
Loc Thang <ldthang@tma.com.vn>
2021-04-13 17:42:23 +0700

    update

--- download.py
+++ download.py
@@ -28,20 +28,74 @@ def getStart():
     f = open("total.txt")
     return int(f.read())
 
-def DownloadFile(path,output):
+def AddID(path):
     with open(path,encoding="utf-8") as fp:
         for line in fp:
-            print("Downdloading: "+line[14:22])
-            download(line[14:22],output)
+            ARR_ID.append(line[14:22])
+            #download(line[14:22],output)
             #time.sleep(1)
 ##################################################################################
+ARR_ID =[]
+
 
 def download(id,output):
-    r = requests.get("http://api.mp3.zing.vn/api/streaming/audio/"+id+"/128")
-    with open(output+"/"+id+".mp3", "wb") as handle:
-        for data in tqdm(r.iter_content()):
-            handle.write(data)
+    try:
+        print(ARR_ID[id])
+    except:
+        print("Done")
+    # r = requests.get("http://api.mp3.zing.vn/api/streaming/audio/"+id+"/128")
+    # with open(output+"/"+id+".mp3", "wb") as handle:
+    #     for data in tqdm(r.iter_content()):
+    #         handle.write(data)
+
+##################################################################################
+
+def process_range(id_range, store,output):
+    """process a number of ids, storing the results in a dict"""
+    if store is None:
+        store = {}
+    for id in id_range:
+        store[id] = download(id, output)
+        #store[id] = id
+
+    return store
+
+
+def threaded_process_range(nthreads, id_range,output):
+    """process the id range in a specified number of threads"""
+    store = {}
+    threads = []
+
+
+    # create the threads
+    for i in range(nthreads):
+        ids = id_range[i::nthreads]
+        t = Thread(target=process_range, args=(ids,store,output))
+        threads.append(t)
+
+
+    # start the threads
+    [ t.start() for t in threads]
+    # wait for the threads to finish
+    [ t.join() for t in threads ]
+
+
+    return store
+
 
 ##################################################################################
 if __name__ == "__main__":
-    DownloadFile("test.txt","Down")
+    AddID("test.txt")
+    print(ARR_ID)
+    START = 0
+    STEP = 10
+    END = len(ARR_ID)
+    while START < END:
+        print("-------------------------------------------")
+        print(str(START)+"----"+(datetime.datetime.now().strftime("%X")))
+        threaded_process_range(STEP,list(range(START,START+STEP)),"Down")
+        START+=STEP
+        writeTotal(START)
+        time.sleep(1)
+
+    #DownloadFile("test.txt","Down")
